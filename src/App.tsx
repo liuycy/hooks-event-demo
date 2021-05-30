aeb84898c3f1db6dcf87326fa4e64c334f0344a7
liuycy <liuycy@yeah.net>
2021-05-30 22:46:13 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,26 @@ import SearchInput from './SearchInput';
 function App() {
   const [params, updateParams] = React.useState({ keyword: '' });
   const [pager, setPager] = React.useState({} as any);
+  const [filter, setFilter] = React.useState({} as any);
   const [output, updateOutput] = React.useState('');
 
-  const handleFetch = (newParams: any, newPager: any) => {
+  const handleFetch = (newParams: any, newPager: any, newFilter: any) => {
     updateOutput(
-      `keyword: ${newParams.keyword}, pager: ${newPager.current}, ${newPager.size}`
+      `keyword: ${newParams.keyword}, pager: ${newPager.current}, ${newPager.size}, filter: ${newFilter.pid}, ${newFilter.id}`
     );
     setPager({ ...newPager, size: ~~(100 * Math.random()) });
+    setFilter({ ...newFilter, id: ~~(10000 * Math.random()) + 10000 });
   };
 
   const handleChange = React.useCallback((obj: any) => {
     setPager((pager: any) => {
-      const newPager = { ...pager, current: 1 };
-      const newParams = { ...obj };
-      updateParams(newParams);
-      handleFetch(newParams, newPager);
+      setFilter((filter: any) => {
+        const newFilter = { ...filter, pid: 0 };
+        const newPager = { ...pager, current: 1 };
+        const newParams = { ...obj };
+        updateParams(newParams);
+        handleFetch(newParams, newPager, newFilter);
+      });
     });
   }, []);
 
