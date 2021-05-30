c8acdc77a866da0d5700c6b4bbfbdd76830d783f
liuycy <liuycy@yeah.net>
2021-05-30 22:47:15 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,62 @@ import * as React from 'react';
 import SearchInput from './SearchInput';
 
 function App() {
-  const [params, updateParams] = React.useState({ keyword: '' });
-  const [pager, setPager] = React.useState({} as any);
-  const [filter, setFilter] = React.useState({} as any);
-  const [output, updateOutput] = React.useState('');
+  const [state, dispatch] = React.useReducer(
+    (state: any, action: any) => {
+      if (action.type === 'params') {
+        return { ...state, params: { ...state.params, ...action.params } };
+      }
+      if (action.type === 'pager') {
+        return { ...state, pager: { ...state.pager, ...action.pager } };
+      }
+      if (action.type === 'filter') {
+        return { ...state, filter: { ...state.filter, ...action.filter } };
+      }
+      if (action.type === 'output') {
+        return {
+          ...state,
+          output: `keyword: ${state.params.keyword}, pager: ${state.pager.current}, ${state.pager.size}, filter: ${state.filter.pid}, ${state.filter.id}`,
+        };
+      }
+
+      return { ...state };
+    },
+    {
+      params: { keyword: '' },
+      pager: {},
+      filter: {},
+      output: '',
+    }
+  );
 
   const handleFetch = (newParams: any, newPager: any, newFilter: any) => {
-    updateOutput(
-      `keyword: ${newParams.keyword}, pager: ${newPager.current}, ${newPager.size}, filter: ${newFilter.pid}, ${newFilter.id}`
-    );
-    setPager({ ...newPager, size: ~~(100 * Math.random()) });
-    setFilter({ ...newFilter, id: ~~(10000 * Math.random()) + 10000 });
+    dispatch({
+      type: 'output',
+    });
+    dispatch({
+      type: 'pager',
+      pager: { ...newPager, size: ~~(100 * Math.random()) },
+    });
+    dispatch({
+      type: 'filter',
+      filter: { ...newFilter, id: ~~(10000 * Math.random() + 10000) },
+    });
   };
 
   const handleChange = React.useCallback((obj: any) => {
-    setPager((pager: any) => {
-      setFilter((filter: any) => {
-        const newFilter = { ...filter, pid: 0 };
-        const newPager = { ...pager, current: 1 };
-        const newParams = { ...obj };
-        updateParams(newParams);
-        handleFetch(newParams, newPager, newFilter);
-      });
-    });
+    const newFilter = { pid: 0 };
+    const newPager = { current: 1 };
+    const newParams = { ...obj };
+    dispatch({ type: 'params', params: newParams });
+    dispatch({ type: 'pager', pager: newPager });
+    dispatch({ type: 'filter', params: newFilter });
+    handleFetch(newParams, newPager, newFilter);
   }, []);
 
   return (
     <div className="App">
       <SearchInput onChange={handleChange} />
-      <div>{output}</div>
+      <div>{state.output}</div>
     </div>
   );
 }
